95f42a278d4bff607732f17bbc8c108c3575bb62
bhushan-madankar <bmadankar0@gmail.com>
2025-06-15 18:52:22 +0530

    saved work

--- Frontend/src/Components/Header.jsx
+++ Frontend/src/Components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
       <div className="flex items-center">
         <Link to="/">
           <img
-            src="/Wooferz Logo.jpg" // Or replace with your actual logo path
+            src="/src/assets/nsd-logo.jpg" // Or replace with your actual logo path
             alt="Logo"
             className="h-12 w-12 rounded-full"
           />
@@ -33,7 +33,7 @@ const Header = () => {
       <div className="flex items-center space-x-4">
         <a
           href="#shop"
-          className="bg-amber-700 text-white px-5 py-2 rounded-full font-bold hover:bg-amber-800 transition"
+          className="bg-blue-700 text-white px-5 py-2 rounded-full font-bold hover:bg-amber-800 transition"
         >
           Shop
         </a>
